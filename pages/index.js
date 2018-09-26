847e80be0c66cdb0b090d71fe438ad2a8150ce3c
mkhizeryounas <m.khizeryounas@gmail.com>
2018-09-26 06:13:03 +0500

    update

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,40 @@
+import { Component } from "react";
+
 import Layout from "../components/MyLayout.js";
 import Link from "next/link";
 import fetch from "isomorphic-unfetch";
 
-const Index = props => (
-  <Layout>
-    <h1>Batman TV Shows</h1>
-    <ul>
-      {props.shows.map(({ show }) => (
-        <li key={show.id}>
-          <Link as={`/p/${show.id}`} href={`/post?id=${show.id}`}>
-            <a>{show.name}</a>
-          </Link>
-        </li>
-      ))}
-    </ul>
-  </Layout>
-);
+export default class Page extends Component {
+  constructor(props) {
+    super();
+  }
+  static async getInitialProps(props) {
+    this.props = props;
 
-Index.getInitialProps = async function() {
-  const res = await fetch("https://api.tvmaze.com/search/shows?q=batman");
-  const data = await res.json();
+    const res = await fetch("https://api.tvmaze.com/search/shows?q=batman");
+    const data = await res.json();
 
-  console.log(`Show data fetched. Count: ${data.length}`);
+    console.log(`Show data fetched. Count: ${data.length}`);
 
-  return {
-    shows: data
-  };
-};
+    return {
+      shows: data
+    };
+  }
 
-export default Index;
+  render() {
+    return (
+      <Layout>
+        <h1>Batman TV Shows</h1>
+        <ul>
+          {this.props.shows.map(({ show }) => (
+            <li key={show.id}>
+              <Link as={`/p/${show.id}`} href={`/post?id=${show.id}`}>
+                <a>{show.name}</a>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </Layout>
+    );
+  }
+}
